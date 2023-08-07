f3429b4ce8174be59786f82f3db31c047446e52c
sauverpro <unsauvechriss@gmail.com>
2023-08-08 00:57:49 +0200

    Resolving selected country appearence

--- js/details.js
+++ js/details.js
@@ -1,30 +1,32 @@
 const countryDetail = document.getElementById("countryDetail");
-
+let countryData = []
 // Function to fetch detailed data for a specific country
 const fetchCountryDetail = async countryCode => {
     try {
         const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
                                     //   https://restcountries.com/v3.1/name/{name}
         const data = await response.json();
-        console.log(data);
+       
         return data;
     } catch (error) {
         console.error("Error fetching country detail:", error);
     }
 };
-
+  
 // Function to render country detail information
 const renderCountryDetail = async countryCode => {
-    const countryData = await fetchCountryDetail(countryCode);
-
+    countryData =await fetchCountryDetail(countryCode);
+     console.log(countryData[0]);
+console.log(`this is country data ${countryData[0]}`);
     // Create detail card and populate with data
     const detailCard = document.createElement("div");
     detailCard.classList.add("country-detail-card");
     detailCard.innerHTML = `
-        <h2>${countryData.name.common}</h2>
-        <p>Population: ${countryData.population}</p>
-        <p>Region: ${countryData.region}</p>
-        <p>Subregion: ${countryData.subregion}</p>
+    <img src="${countryData[0].flags.svg}">
+        <h2>${countryData[0].name.common}</h2>
+        <p>Population: ${countryData[0].population}</p>
+        <p>Region: ${countryData[0].region}</p>
+        <p>Subregion: ${countryData[0].subregion}</p>
         <!-- Add more details as needed -->
     `;
 
